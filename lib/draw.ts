aac48afdf60bf86921ebbadb53043d84e2cf055e
swayams1 <swayam.siddha@tieto.com>
2020-05-18 15:54:11 +0530

    basic layout using classes

--- lib/draw.ts
+++ lib/draw.ts
@@ -9,8 +9,13 @@ const document = new JSDOM('html').window.document;
 const root: HTMLDivElement = document.createElement("div");
 root.setAttribute("id", "root");
 
+let link: HTMLElement = document.createElement("link")
+link.setAttribute( "href", "../templates/layouts.css")
+link.setAttribute("rel", "stylesheet")
+root.appendChild(link)
+
 const draw = (element: any, root: HTMLDivElement) => {
-  const current = createNode(element);
+  const current = createNodeWithClass(element);
   root.appendChild(current);
   if (element.children.length > 0)
     element.children.forEach((child: Element) => draw(child, current));
@@ -21,6 +26,14 @@ const createNode = (element: Element) => {
   let elem = document.createElement("div");
   elem.setAttribute("name", element.name);
   elem.setAttribute("style", flex.get())
+  return elem;
+};
+
+const createNodeWithClass = (element: Element) => {
+  let flex = new Flex('column')
+  let elem = document.createElement("div");
+  elem.setAttribute("name", element.name);
+  elem.setAttribute("class", "grid")
   return elem;
 };
 
